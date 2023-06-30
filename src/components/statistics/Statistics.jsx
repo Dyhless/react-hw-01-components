673c9d000cd3f606e01edc64da1ef4f35730529b
Dmytro Medvediev <dsisitkin@gmail.com>
2023-06-30 18:01:51 +0200

    update

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,18 +1,21 @@
 import data from './data.json';
 
 export const Statistics = () => {
-  return (
-    <section className="statistics">
-      <h2 className="title">Upload stats</h2>
+   return (
+      <section className="statistics">
+         <div className='stat-block'>
+         <h2 className="title">Upload stats</h2>
 
-      <ul className="stat-list">
-        {data.map(stats => (
-          <li key={stats.id} className="item">
-            <span className="label">{stats.label}</span>
-            <span className="percentage">{stats.percentage}</span>
-          </li>
-        ))}
-      </ul>
-    </section>
+         <ul className="stat-list">
+            {data.map(stats => (
+               <li key={stats.id} className="item">
+               <span className="label lbl-txt"> {stats.label} %</span>
+               <span className="percentage"> {stats.percentage} %</span>
+               </li>
+            ))}
+         </ul>
+         </div>
+      
+      </section>  
   );
 };
