0d620bf3f7b15f54dec90158c9e3c16fc95b2964
Dmytro Medvediev <dsisitkin@gmail.com>
2023-06-30 16:35:12 +0200

    update folders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,9 @@
-import user from './card/user.json';
-import './card/card.css';
+import { Card } from './card/Card';
 
 export const App = () => {
   return (
-
-  <div className="profile">
-    <div className="description">
-      <img
-        src={user.avatar}
-        alt="User avatar"
-        className="avatar"
-      />
-      <p className="name">{user.username}</p>
-      <p className="tag">@{user.tag}</p>
-      <p className="location">{user.location}</p>
+    <div>
+      <Card />
     </div>
-
-    <ul className="stats">
-      <li className='stats-item'>
-        <span className="label">Followers </span>
-        <span className="quantity">{user.stats.followers}</span>
-      </li>
-      <li className='stats-item'>
-        <span className="label">Views </span>
-        <span className="quantity">{user.stats.views}</span>
-      </li>
-      <li className='stats-item'>
-        <span className="label">Likes </span>
-        <span className="quantity">{user.stats.likes}</span>
-      </li>
-    </ul>
-  </div>
   );
 };
