32e6e40a85ee25e792e5f4371c13b28d4c617e1c
Dmytro Medvediev <dsisitkin@gmail.com>
2023-06-30 13:43:58 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,14 @@ import user from './user.json';
 export const App = () => {
   return (
 
-  <div className="profile"
-    style={{
+  <div className="card"
+      style={{
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
     }}>
     <div className="description">
       <img
@@ -19,22 +19,22 @@ export const App = () => {
         className="avatar"
       />
       <p className="name">{user.username}</p>
-      <p className="tag">{user.tag}</p>
+      <p className="tag">@{user.tag}</p>
       <p className="location">{user.location}</p>
     </div>
 
     <ul className="stats">
       <li>
-        <span className="label">Followers</span>
-        <span className="quantity">{user.followers}</span>
+        <span className="label">Followers </span>
+        <span className="quantity">{user.stats.followers}</span>
       </li>
       <li>
-        <span className="label">Views</span>
-        <span className="quantity">{user.views}</span>
+        <span className="label">Views </span>
+        <span className="quantity">{user.stats.views}</span>
       </li>
       <li>
-        <span className="label">Likes</span>
-        <span className="quantity">{user.likes}</span>
+        <span className="label">Likes </span>
+        <span className="quantity">{user.stats.likes}</span>
       </li>
     </ul>
   </div>
