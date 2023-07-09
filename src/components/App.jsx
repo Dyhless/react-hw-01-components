3f7751585b4d25db6753f1bfec62001f742ec2f1
Dmytro Medvediev <dsisitkin@gmail.com>
2023-07-09 13:58:16 +0200

    update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,26 @@
-import { Profile } from './profile/Profile';
-import { Statistics } from './statistics/Statistics';
+import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
-import { Transactions } from './transactions/Transactions.jsx';
+import { Transactions } from './Transactions/Transactions.jsx';
+
+import user from './Profile/user.json';
+import data from './Statistics/data.json';
+import friends from './FriendList/friends.json'; 
+import transactions from './Transactions/transactions.json';
 
 export const App = () => {
   return (
-    <div>
-      <Profile />
-      <Statistics />
-      <FriendList />
-      <Transactions />
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <Transactions items={transactions} />
+    </>
   );
 };
--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,41 +1,41 @@
-import user from './user.json';
+import PropTypes from 'prop-types';
 
-export const Profile = () => {
+export const Profile = ({ avatar, username, location, stats, tag }) => {
   return (
     <div className="profile">
       <div className="description">
-        <img src={user.avatar} alt="User avatar" className="avatar" />
-        <p className="name">{user.username}</p>
-        <p className="tag">@{user.tag}</p>
-        <p className="location">{user.location}</p>
+        <img src={avatar} alt="User avatar" className="avatar" />
+        <p className="name">{username}</p>
+        <p className="tag">@{tag}</p>
+        <p className="location">{location}</p>
       </div>
 
       <ul className="stats">
         <li className="stats-item">
           <span className="label">Followers </span>
-          <span className="quantity">{user.stats.followers}</span>
+          <span className="quantity">{stats.followers}</span>
         </li>
         <li className="stats-item">
           <span className="label">Views </span>
-          <span className="quantity">{user.stats.views}</span>
+          <span className="quantity">{stats.views}</span>
         </li>
         <li className="stats-item">
           <span className="label">Likes </span>
-          <span className="quantity">{user.stats.likes}</span>
+          <span className="quantity">{stats.likes}</span>
         </li>
       </ul>
     </div>
   );
 };
 
-// Profile.propTypes = {
-//   avatar: PropTypes.string.isRequired,
-//   username: PropTypes.string.isRequired,
-//   location: PropTypes.string.isRequired,
-//   stats: PropTypes.shape({
-//     followers: PropTypes.number.isRequired,
-//     views: PropTypes.number.isRequired,
-//     likes: PropTypes.number.isRequired,
-//   }),
-//   tag: PropTypes.string.isRequired,
-// };
+Profile.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
+  tag: PropTypes.string.isRequired,
+};
