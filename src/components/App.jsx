d76f4a3e9172ff58fc6f646f0f908bb277ce1660
Dmytro Medvediev <dsisitkin@gmail.com>
2023-07-09 16:41:25 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
-import { Profile } from './Profile/Profile.jsx';
+
 import { Statistics } from './Statistics/Statistics';
+import { Profile } from './Profile/Profile.jsx';
 import { FriendList } from './FriendList/FriendList';
 import { Transactions } from './Transactions/Transactions.jsx';
+
 import user from './Profile/user.json';
 import data from './Statistics/data.json';
 import friends from './FriendList/friends.json'; 
